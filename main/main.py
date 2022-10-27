782dc09079b62d4b62a6a84a5c076677eab40a77
hendarta0318 <hendarta0318@gmail.com>
2022-10-27 17:47:27 +0700

    revisi_arena

--- main/main.py
+++ main/main.py
@@ -4,7 +4,7 @@ from OpenGL.GLU import *
 
 w,h= 1360,1000
 
-def cover():
+def arena_labirin():
     glBegin(GL_QUADS)#atas
     glVertex2f(150, 600)#kiri atas, kiri bawah
     glVertex2f(820, 600)#<=kanan bawah, kanan atas
@@ -13,18 +13,38 @@ def cover():
     glEnd()
 
     glBegin(GL_QUADS)#kiri1
-    glVertex2f(180, 200)#kiri atas, kiri bawah
-    glVertex2f(150, 200)#<=kanan bawah, kanan atas
+    glVertex2f(180, 70)#kiri atas, kiri bawah
+    glVertex2f(150, 70)#<=kanan bawah, kanan atas
     glVertex2f(150, 600)#<=kanan atas, kanan bawah
     glVertex2f(180, 600)#kiri bawah, kiri atas
     glEnd()
 
     glBegin(GL_QUADS)#kiri2
-    glVertex2f(270, 300)#kiri atas, kiri bawah
-    glVertex2f(300, 300)#<=kanan bawah, kanan atas
+    glVertex2f(270, 370)#kiri atas, kiri bawah
+    glVertex2f(300, 370)#<=kanan bawah, kanan atas
     glVertex2f(300, 600)#<=kanan atas, kanan bawah
     glVertex2f(270, 600)#kiri bawah, kiri atas
     glEnd()
+
+    glBegin(GL_QUADS)#bawah_kiri2
+    glVertex2f(150, 300)#kiri atas, kiri bawah
+    glVertex2f(470, 300)#<=kanan bawah, kanan atas
+    glVertex2f(470, 270)#<=kanan atas, kanan bawah
+    glVertex2f(150, 270)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#bawah_bawah_kiri2
+    glVertex2f(150, 190)#kiri atas, kiri bawah
+    glVertex2f(470, 190)#<=kanan bawah, kanan atas
+    glVertex2f(470, 160)#<=kanan atas, kanan bawah
+    glVertex2f(150, 160)#kiri bawah, kiri atas
+    glEnd()
+
+    # glBegin(GL_QUADS)#bawah_bawah_kiri2
+    # glVertex2f(150, 300)#kiri atas, kiri bawah
+    # glVertex2f(470, 300)#<=kanan bawah, kanan atas
+    # glVertex2f(470, 270)#<=kanan atas, kanan bawah
+    # glVertex2f(150, 270)#kiri bawah, kiri atas
 
 def iterate():
     glViewport(0, 0, 1360, 1000)
@@ -40,7 +60,7 @@ def showScreen():
     glLoadIdentity()
     iterate()
     glColor3f(0.0, 0.0, 0.0)
-    cover()
+    arena_labirin()
     glutSwapBuffers()
 
 glutInit()
