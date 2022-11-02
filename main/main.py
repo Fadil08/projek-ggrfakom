ebe75f95f990beb87209e4be943b4da96dc3fc25
hendarta0318 <hendarta0318@gmail.com>
2022-11-03 05:00:55 +0700

    updateArenake3

--- main/main.py
+++ main/main.py
@@ -6,50 +6,50 @@ w,h= 1360,1000
 
 def arena_labirin():
     glBegin(GL_QUADS)#atas
-    glVertex2f(150, 1600)#kiri atas, kiri bawah
-    glVertex2f(1600, 1600)#<=kanan bawah, kanan atas
-    glVertex2f(1600, 1570)#<=kanan atas, kanan bawah
-    glVertex2f(150, 1570)#kiri bawah, kiri atas
+    glVertex2f(150, 1660)#kiri atas, kiri bawah
+    glVertex2f(1600, 1660)#<=kanan bawah, kanan atas
+    glVertex2f(1600, 1630)#<=kanan atas, kanan bawah
+    glVertex2f(150, 1630)#kiri bawah, kiri atas
     glEnd()
 
     glBegin(GL_QUADS)#kiri1
-    glVertex2f(180, 1600)#kiri atas, kiri bawah
-    glVertex2f(150, 1600)#<=kanan bawah, kanan atas
-    glVertex2f(150, 710)#<=kanan atas, kanan bawah
-    glVertex2f(180, 710)#kiri bawah, kiri atas
+    glVertex2f(180, 1660)#kiri atas, kiri bawah
+    glVertex2f(150, 1660)#<=kanan bawah, kanan atas
+    glVertex2f(150, 630)#<=kanan atas, kanan bawah
+    glVertex2f(180, 630)#kiri bawah, kiri atas
     glEnd()
 
     glBegin(GL_QUADS)#kiri2
-    glVertex2f(270, 1600)#kiri atas, kiri bawah
-    glVertex2f(300, 1600)#<=kanan bawah, kanan atas
-    glVertex2f(300, 1150)#<=kanan atas, kanan bawah
-    glVertex2f(270, 1150)#kiri bawah, kiri atas
+    glVertex2f(330, 1660)#kiri atas, kiri bawah
+    glVertex2f(360, 1660)#<=kanan bawah, kanan atas
+    glVertex2f(360, 1142)#<=kanan atas, kanan bawah
+    glVertex2f(330, 1142)#kiri bawah, kiri atas
     glEnd()
 
-    glBegin(GL_QUADS)#bawah_kiri2
-    glVertex2f(150, 1060)#kiri atas, kiri bawah
-    glVertex2f(500, 1060)#<=kanan bawah, kanan atas
-    glVertex2f(500, 1030)#<=kanan atas, kanan bawah
-    glVertex2f(150, 1030)#kiri bawah, kiri atas
+    glBegin(GL_QUADS)#bawah1
+    glVertex2f(150, 1000)#kiri atas, kiri bawah
+    glVertex2f(560, 1000)#<=kanan bawah, kanan atas
+    glVertex2f(560, 970)#<=kanan atas, kanan bawah
+    glVertex2f(150, 970)#kiri bawah, kiri atas
     glEnd()
 
-    glBegin(GL_QUADS)#bawah_bawah_kiri2
-    glVertex2f(150, 900)#kiri atas, kiri bawah
-    glVertex2f(400, 900)#<=kanan bawah, kanan atas
-    glVertex2f(400, 870)#<=kanan atas, kanan bawah
-    glVertex2f(150, 870)#kiri bawah, kiri atas
+    glBegin(GL_QUADS)#bawah2
+    glVertex2f(150, 830)#kiri atas, kiri bawah
+    glVertex2f(400, 830)#<=kanan bawah, kanan atas
+    glVertex2f(400, 800)#<=kanan atas, kanan bawah
+    glVertex2f(150, 800)#kiri bawah, kiri atas
     glEnd()
 
-    glBegin(GL_QUADS)#bawah_bawah_bawah_kiri2
-    glVertex2f(150, 740)#kiri atas, kiri bawah
-    glVertex2f(580, 740)#<=kanan bawah, kanan atas
-    glVertex2f(580, 710)#<=kanan atas, kanan bawah
-    glVertex2f(150, 710)#kiri bawah, kiri atas
+    glBegin(GL_QUADS)#bawah3
+    glVertex2f(150, 660)#kiri atas, kiri bawah
+    glVertex2f(580, 660)#<=kanan bawah, kanan atas
+    glVertex2f(580, 630)#<=kanan atas, kanan bawah
+    glVertex2f(150, 630)#kiri bawah, kiri atas
     glEnd()
 
     glBegin(GL_QUADS)#kiri3
-    glVertex2f(580, 710)#kiri atas, kiri bawah
-    glVertex2f(550, 710)#<=kanan bawah, kanan atas
+    glVertex2f(580, 650)#kiri atas, kiri bawah
+    glVertex2f(550, 650)#<=kanan bawah, kanan atas
     glVertex2f(550, 500)#<=kanan atas, kanan bawah
     glVertex2f(580, 500)#kiri bawah, kiri atas
     glEnd()
@@ -63,8 +63,8 @@ def arena_labirin():
 
     glBegin(GL_QUADS)#lanjutan2 bawah_bawah_kiri2
     glVertex2f(550, 330)#kiri atas, kiri bawah
-    glVertex2f(800, 330)#<=kanan bawah, kanan atas
-    glVertex2f(800, 300)#<=kanan atas, kanan bawah
+    glVertex2f(700, 330)#<=kanan bawah, kanan atas
+    glVertex2f(700, 300)#<=kanan atas, kanan bawah
     glVertex2f(550, 300)#kiri bawah, kiri atas
     glEnd()
 
@@ -75,7 +75,7 @@ def arena_labirin():
     glVertex2f(550, 150)#kiri bawah, kiri atas
     glEnd()
 
-    glBegin(GL_QUADS)#lanjutan2 bawah_bawah_kiri2
+    glBegin(GL_QUADS)#bawah sendiri
     glVertex2f(550, 180)#kiri atas, kiri bawah
     glVertex2f(1500, 180)#<=kanan bawah, kanan atas
     glVertex2f(1500, 150)#<=kanan atas, kanan bawah
@@ -83,24 +83,88 @@ def arena_labirin():
     glEnd()
 
     glBegin(GL_QUADS)#atastengah
-    glVertex2f(800, 1600)#kiri atas, kiri bawah
-    glVertex2f(770, 1600)#<=kanan bawah, kanan atas
-    glVertex2f(770, 1420)#<=kanan atas, kanan bawah
-    glVertex2f(800, 1420)#kiri bawah, kiri atas
+    glVertex2f(800, 1660)#kiri atas, kiri bawah
+    glVertex2f(770, 1660)#<=kanan bawah, kanan atas
+    glVertex2f(770, 1470)#<=kanan atas, kanan bawah
+    glVertex2f(800, 1470)#kiri bawah, kiri atas
     glEnd()
 
     glBegin(GL_QUADS)#lanjutatastengah
-    glVertex2f(550, 1420)#kiri atas, kiri bawah
-    glVertex2f(800, 1420)#<=kanan bawah, kanan atas
-    glVertex2f(800, 1390)#<=kanan atas, kanan bawah
-    glVertex2f(550, 1390)#kiri bawah, kiri atas
+    glVertex2f(485, 1500)#kiri atas, kiri bawah
+    glVertex2f(800, 1500)#<=kanan bawah, kanan atas
+    glVertex2f(800, 1470)#<=kanan atas, kanan bawah
+    glVertex2f(485, 1470)#kiri bawah, kiri atas
     glEnd()
 
-    glBegin(GL_QUADS)#lanjutatastengah2
-    glVertex2f(550, 1260)#kiri atas, kiri bawah
-    glVertex2f(800, 1260)#<=kanan bawah, kanan atas
-    glVertex2f(800, 1230)#<=kanan atas, kanan bawah
-    glVertex2f(550, 1230)#kiri bawah, kiri atas
+
+    glBegin(GL_QUADS)#lanjutan bawah1
+    glVertex2f(550, 1000)#kiri atas, kiri bawah
+    glVertex2f(580, 1000)#<=kanan bawah, kanan atas
+    glVertex2f(580, 800)#<=kanan atas, kanan bawah
+    glVertex2f(550, 800)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#atas ke 2
+    glVertex2f(515, 1332)#kiri atas, kiri bawah
+    glVertex2f(800, 1332)#<=kanan bawah, kanan atas
+    glVertex2f(800, 1302)#<=kanan atas, kanan bawah
+    glVertex2f(515, 1302)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#penghubung 2,3
+    glVertex2f(485, 1332)#kiri atas, kiri bawah
+    glVertex2f(515, 1332)#<=kanan bawah, kanan atas
+    glVertex2f(515, 1142)#<=kanan atas, kanan bawah
+    glVertex2f(485, 1142)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#atas ke 3
+    glVertex2f(515, 1172)#kiri atas, kiri bawah
+    glVertex2f(930, 1172)#<=kanan bawah, kanan atas
+    glVertex2f(930, 1142)#<=kanan atas, kanan bawah
+    glVertex2f(515, 1142)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#Tengah1
+    glVertex2f(720, 1000)#kiri atas, kiri bawah
+    glVertex2f(1250, 1000)#<=kanan bawah, kanan atas
+    glVertex2f(1250, 970)#<=kanan atas, kanan bawah
+    glVertex2f(720, 970)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#lanjutan atas ke 3
+    glVertex2f(930, 1172)#kiri atas, kiri bawah
+    glVertex2f(960, 1172)#<=kanan bawah, kanan atas
+    glVertex2f(960, 970)#<=kanan atas, kanan bawah
+    glVertex2f(930, 970)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#kanannya penghubung 2,3
+    glVertex2f(1080, 1332)#kiri atas, kiri bawah
+    glVertex2f(1110, 1332)#<=kanan bawah, kanan atas
+    glVertex2f(1110, 1142)#<=kanan atas, kanan bawah
+    glVertex2f(1080, 1142)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#kanannya atas ke 2
+    glVertex2f(930, 1332)#kiri atas, kiri bawah
+    glVertex2f(1110, 1332)#<=kanan bawah, kanan atas
+    glVertex2f(1110, 1302)#<=kanan atas, kanan bawah
+    glVertex2f(930, 1302)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#atasnya tengah 1
+    glVertex2f(930, 1500)#kiri atas, kiri bawah
+    glVertex2f(1110, 1500)#<=kanan bawah, kanan atas
+    glVertex2f(1110, 1470)#<=kanan atas, kanan bawah
+    glVertex2f(930, 1470)#kiri bawah, kiri atas
+    glEnd()
+
+    glBegin(GL_QUADS)#bawah tengah 1
+    glVertex2f(1080, 1000)#kiri atas, kiri bawah
+    glVertex2f(1110, 1000)#<=kanan bawah, kanan atas
+    glVertex2f(1110, 315)#<=kanan atas, kanan bawah
+    glVertex2f(1080, 315)#kiri bawah, kiri atas
     glEnd()
 
 def iterate():
